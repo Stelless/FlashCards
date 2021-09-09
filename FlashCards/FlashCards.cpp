ce2e6e1de1869eb548501b22ba0476bba547d13c
Stelless <47225505+Stelless@users.noreply.github.com>
2021-09-09 13:49:50 -0700

    Added quit functionality to exercise loop

--- FlashCards/FlashCards.cpp
+++ FlashCards/FlashCards.cpp
@@ -93,14 +93,15 @@ void print_option_menu() {
 void cycle_current_deck(Deck flash_card_deck) {
    system("CLS");
    struct exercise_state {
-      bool ex_is_active{ false };
+      bool ex_is_active{ true };
       char user_input{};
       int num_cards_held{ 0 };
       int current_card_num{ 0 };
 
       void print_instructions() { std::cout << "After viewing the id & question please press enter to view the answer." << std::endl; }
       void print_ex_progress(std::map<std::string, std::shared_ptr<Card>> deck, std::map<std::string, std::shared_ptr<Card>> hold_deck) { std::cout << "Card: " << current_card_num << " out of: " << deck.size() << " cards." << " There are " << hold_deck.size() << " cards in the hold deck." << std::endl; }
-      //void print_key_option_menu() {q=='exit exercise', 0=="hold card and move to next", "enter"="move to next" } // maybe implement?
+      void print_key_option_menu() { std::cout << "[Q]: Exit, [0]: Hold Card, [Enter]: Proceed to Next Card" << std::endl; } // maybe implement?
+      void print_hold_key_option_menu() { cout << "[Q]: Exit, [Enter]: Proceed to Next Card" << endl; }
       void print_hold_instructions() { std::cout << "Press '0' if you would like to hold this question to be reviewed at the end otherwise press 'enter' to remove it from the deck and show the next card." << std::endl; }
       void print_beg_hold_message() { std::cout << "Beginning hold deck cycle." << std::endl; }
       void print_hold_ex_progress(int current_index, std::map<std::string, std::shared_ptr<Card>> hold_deck) { cout << "Card: " << current_index << " out of: " << hold_deck.size() << " cards." << endl; }
@@ -108,59 +109,73 @@ void cycle_current_deck(Deck flash_card_deck) {
       void print_continue_message() { cout << "Press enter to continue" << endl; }
    };
 
-   exercise_state e_state;
+   exercise_state e_state{ true };
    Deck hold_deck{};
    std::cin.ignore(256, '\n'); // clears the cin.get() cache? I have no idea -.- @@@@@@@@@@
 
    for (const auto& card : flash_card_deck.deck) {
-      e_state.ex_is_active = true; // do something with this
-      ++e_state.current_card_num;
-      e_state.print_instructions();
-      e_state.print_ex_progress(flash_card_deck.deck, hold_deck.deck);
-      std::cout << "\n----------------------------------------------------------------" << std::endl;
-      card.second->print_id();
-      card.second->print_question();
-      cin.get(); // This will 'pause' the exercise and allow time to answer the question.
+      if (e_state.ex_is_active == true) {
+         ++e_state.current_card_num;
+         e_state.print_instructions();
+         e_state.print_ex_progress(flash_card_deck.deck, hold_deck.deck);
+         std::cout << "\n----------------------------------------------------------------" << std::endl;
+         card.second->print_id();
+         card.second->print_question();
+         cin.get(); // This will 'pause' the exercise and allow time to answer the question.
 
 
-      card.second->print_ans();
-      std::cout << "----------------------------------------------------------------\n" << endl;
-      e_state.print_hold_instructions();
-      e_state.user_input = _getch(); // 'pause' the exercise and wait for user input
+         card.second->print_ans();
+         std::cout << "----------------------------------------------------------------\n" << endl;
+         e_state.print_hold_instructions();
+         e_state.print_key_option_menu();
+         e_state.user_input = _getch(); // 'pause' the exercise and wait for user input
 
 
-      if (e_state.user_input == '0') {
-         hold_deck.addCard(*card.second);
-         ++e_state.num_cards_held;
+         if (e_state.user_input == '0') {
+            hold_deck.addCard(*card.second);
+            ++e_state.num_cards_held;
+         }
+         else if (e_state.user_input == 'q' || e_state.user_input == 'Q') {
+            e_state.ex_is_active = false;
+            break;
+         }
+         system("CLS");
       }
-      system("CLS");
    }
-   e_state.print_final_report(flash_card_deck.deck);
-   e_state.print_beg_hold_message();
-   cin.get();
-
-   //start hold loop here? -simplified version with no hold
-   int card_index{0};
-
-   for (const auto& card : hold_deck.deck) {
-      ++card_index;
-      e_state.print_instructions();
-      e_state.print_hold_ex_progress(card_index, hold_deck.deck);
-
-      std::cout << "\n----------------------------------------------------------------" << std::endl;
-      card.second->print_id();
-      card.second->print_question();
-      cin.get(); // This will 'pause' the exercise and allow time to answer the question.
-
-
-      card.second->print_ans();
-      std::cout << "----------------------------------------------------------------\n" << endl;
-      e_state.print_continue_message();
+   if (e_state.ex_is_active == true) {
+      e_state.print_final_report(flash_card_deck.deck);
+      e_state.print_beg_hold_message();
       cin.get();
-      system("CLS");
-   }
 
+      //start hold loop here? -simplified version with no hold
+      int card_index{ 0 };
+
+      for (const auto& card : hold_deck.deck) {
+         if (e_state.ex_is_active == true) {
+            ++card_index;
+            e_state.print_instructions();
+            e_state.print_hold_ex_progress(card_index, hold_deck.deck);
+
+            std::cout << "\n----------------------------------------------------------------" << std::endl;
+            card.second->print_id();
+            card.second->print_question();
+            cin.get();
+
+            card.second->print_ans();
+            std::cout << "----------------------------------------------------------------\n" << endl;
+            e_state.print_continue_message();
+            e_state.print_hold_key_option_menu();
+
+            e_state.user_input = _getch();
+            if (e_state.user_input == 'q' || e_state.user_input == 'Q') {
+               e_state.ex_is_active = false;
+               break;
+            }
+            system("CLS");
+         }
+      }
+   }
    cout << "Exiting cycle_current_deck, press enter to continue........." << endl;
    cin.get(); // 'pauses' before returniung to the main menu
-
+   system("CLS");
 }
